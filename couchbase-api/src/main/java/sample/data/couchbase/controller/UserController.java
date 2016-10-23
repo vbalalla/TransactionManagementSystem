8a8f2ce82379af77221c0421474ff80175cec4cc
vbalalla <vbalalla@gmail.com>
2016-10-23 20:28:06 +0530

    Update: Changing Dependencies.

--- couchbase-api/src/main/java/sample/data/couchbase/controller/UserController.java
+++ couchbase-api/src/main/java/sample/data/couchbase/controller/UserController.java
@@ -1,9 +1,12 @@
 package sample.data.couchbase.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import sample.data.couchbase.api.User;
+import sample.data.couchbase.model.UserRepository;
 
 /**public class SampleController {
 
@@ -21,12 +24,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * Created by vibodha on 10/22/16.
  */
 
-@Controller
+@RestController
+@EnableAutoConfiguration
 public class UserController {
-    @GetMapping("/")
-    @ResponseBody
-    @Transactional(readOnly = true)
+
+    @Autowired
+    UserRepository userRepository;
+
+    @RequestMapping("/")
     public String helloWorld() {
         return "Hello world";
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public User addUser(@RequestBody User user){
+        userRepository.save(user);
+        return user;
     }
 }
